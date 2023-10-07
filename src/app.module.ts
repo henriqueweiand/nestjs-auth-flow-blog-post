8c764d814c8ca707c214581300fc11fbd3281d72
henrique weiand <henriqueweiand@gmail.com>
2023-10-07 16:39:11 -0300

    added database connection module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,17 @@ import { AppService } from './app.service';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+// import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [
-    DevtoolsModule.register({
-      http: process.env.NODE_ENV !== 'production',
-    }),
-    AuthModule,
-    UsersModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        DevtoolsModule.register({
+            http: process.env.NODE_ENV !== 'production',
+        }),
+        AuthModule,
+        UsersModule,
+        // DatabaseModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/database/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'root',
+            database: 'project',
+            entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+            migrations: [`${__dirname}/migrations/*{.ts,.js}`],
+            synchronize: false,
+            migrationsRun: true,
+            logging: true,
+        }),
+    ],
+})
+export class DatabaseModule { }
